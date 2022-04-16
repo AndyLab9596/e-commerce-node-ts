152a0bcd3a3e0152f7dab523ba0541388bf07464
andy <vy.chung@hdwebsoft.co>
2022-04-16 14:51:24 +0700

    fixing types middleware

--- src/app.ts
+++ src/app.ts
@@ -25,7 +25,7 @@ app.get('/', (req: Request, res: Response) => {
 })
 
 app.use('/api/v1/auth', authRouter)
-app.use('/api/v1/users',authenticateUser as any, userRouter)
+app.use('/api/v1/users',authenticateUser, userRouter)
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
