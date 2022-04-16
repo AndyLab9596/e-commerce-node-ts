acee535ade7ca6c0f8d537627a4ecdccce6f77b9
andy <vy.chung@hdwebsoft.co>
2022-04-16 07:23:53 +0700

    connect to db

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,24 @@
-console.log('Hello world')
+import express, { Application, Request, Response } from 'express';
+import dotenv from 'dotenv';
+import "express-async-errors";
+import connectDb from './db/connect';
+
+dotenv.config()
+const app: Application = express();
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello world')
+})
+
+const port = process.env.PORT || 5000;
+const start = async () => {
+    try {
+        await connectDb(process.env.MONGO_URL as string)
+        app.listen(port, () => {
+            console.log(`Server is running on port ${port}...`)
+        })
+    } catch (err) {
+        console.log(err)
+    }
+}
+start()
